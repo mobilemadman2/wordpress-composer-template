a47033f598924f628efd2949772889657b11f313
Nik <nik@mail.ru>
2018-12-06 12:59:47 +0500

    Move DB settings to new file

--- wp-config.php
+++ wp-config.php
@@ -23,23 +23,7 @@ require __DIR__ . '/wp-content/vendor/autoload.php';
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'wordpress_composer');
-
-/** MySQL database username */
-define('DB_USER', 'root');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'root');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+require_once dirname(__FILE__) . '/db-config.php';
 
 /**#@+
  * Authentication Unique Keys and Salts.
