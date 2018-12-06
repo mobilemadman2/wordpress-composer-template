280a78638d61fc378342137deb58c3ffe9c0a55a
Nik <nik@mail.ru>
2018-12-06 15:13:34 +0500

    Update instruction

--- wp-config.php
+++ wp-config.php
@@ -1,8 +1,4 @@
 <?php
-
-// We add autoload of vendor's libs
-require __DIR__ . '/wp-content/vendor/autoload.php';
-
 /**
  * The base configuration for WordPress
  *
@@ -23,7 +19,23 @@ require __DIR__ . '/wp-content/vendor/autoload.php';
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
-require_once dirname(__FILE__) . '/db-config.php';
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress_composer');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -34,14 +46,14 @@ require_once dirname(__FILE__) . '/db-config.php';
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'uCw?c.A9CYHRWoAcxaZM5d`j`{Nt#:q##N0MJnA7{Qc4jMaUAW!aSH>ZK(Zn+7yY');
+define('SECURE_AUTH_KEY',  'aOK{jo:zq_-}4@gSQ4iQmp+9R3*f38lz$lU.K^q{Z8O-mo%r&:  3kMYJrYJ<?Sx');
+define('LOGGED_IN_KEY',    '<6TZ|hTpy bn@yR{#*2tdniJQUWqiiLRx}Hl}Z;W~rFq+I%(Hi8p~<Bz.}|Sr`!-');
+define('NONCE_KEY',        'g4LVPQC$@YM$8H%~U/j#g40HR9S^x>cl3d0b1:IBr>`B(c8N=AV l|a]0C@czSz6');
+define('AUTH_SALT',        'X9JYZzQu)a9+}M[zD-eTN`ID<q^qH3$[}X+^wIq<Jd%T&Y7$tuog<t@ngU6CP9R8');
+define('SECURE_AUTH_SALT', '=ykcqPSfmg@/o$pze4U[hMBPnVgpr}2:~69m~6+<l6z;|n:SqUl+ cr3Rma54o{G');
+define('LOGGED_IN_SALT',   'vc>c-x#ec(RCDc[awhn>%+8jMx{+OA{i~esv[%qLt5:G<g~d4x8>=XR`SBrK^nNz');
+define('NONCE_SALT',       'yuL=r ^`PrlBdrIe^/bQ 5|}V>y7[^u]j+,js[V3+H@cNvzCE!O{V|dSWX=7*Gt>');
 
 /**#@-*/
 
@@ -67,6 +79,7 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+
 // Define site URL
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
@@ -81,6 +94,7 @@ define('WP_PLUGIN_DIR', dirname(__FILE__) . '/wp-content/plugins');
 define('WP_PLUGIN_URL', SITE_URL . '/wp-content/plugins');
 // Define this constant to solve possible compatability issues
 define('PLUGINDIR', WP_PLUGIN_URL);
+
 
 /* That's all, stop editing! Happy blogging. */
 
